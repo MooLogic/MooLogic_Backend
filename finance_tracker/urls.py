ad998b2b2dfc1de213073bed3169a15cdda3c371
minekeet <gulumamekonennen24@gmail.com>
2025-06-02 21:46:59 +0300

    updtaed ml and finance

--- finance_tracker/urls.py
+++ finance_tracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import (
 create_income,update_income,delete_income,total_income,income_breakdown,list_income,
-create_expense,update_expense,delete_expense,total_expense,expense_breakdown,list_expense,generate_profit_snapshot,generate_profit_snapshot_with_alerts
+create_expense,update_expense,delete_expense,total_expense,expense_breakdown,list_expense,generate_profit_snapshot,generate_profit_snapshot_with_alerts, monthly_finance_summary,export_financial_records
 
 )
 
@@ -12,15 +12,17 @@ urlpatterns = [
     path('income/delete/<int:income_id>/', delete_income, name='delete_income'),
     path('income/total/', total_income, name='total_income'),
     path('income/breakdown/<int:farm_id>/', income_breakdown, name='income_breakdown'),
-    path('income/list//', list_income, name='list_income'),
+    path('income/list/', list_income, name='list_income'),
 
     path('expense/create/', create_expense, name='create_expense'),
     path('expense/update/<int:expense_id>/', update_expense, name='update_expense'),
     path('expense/delete/<int:expense_id>/', delete_expense, name='delete_expense'),
     path('expense/total/', total_expense, name='total_expense'),
     path('expense/breakdown/<int:farm_id>/', expense_breakdown, name='expense_breakdown'),
-    path('expense/list//', list_expense, name='list_expense'),
+    path('expense/list/', list_expense, name='list_expense'),
     path('profit/snapshot/', generate_profit_snapshot, name='generate_profit_snapshot'),
     path('profit/snapshot/alerts/', generate_profit_snapshot_with_alerts, name='generate_profit_snapshot_with_alerts'),
+    path('monthly-summary/', monthly_finance_summary, name='monthly_finance_summary'),
+    path('export/', export_financial_records, name='export_financial_records'),
 
 ]
